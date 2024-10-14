52092dce0d0b274c525026997e3060012f042899
Hooman <denis_paramon@icloud.com>
2024-10-14 12:40:28 +0500

    initialCommit

---
+++ my_project/src/Controller/CarApiController.php
@@ -0,0 +1,39 @@
+<?php
+// src/Controller/CarApiController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CarApiController extends AbstractController
+{
+    // Получение списка автомобилей
+    #[Route('/api/v1/cars', name: 'get_all_cars', methods: ['GET'])]
+    public function getAllCars(): JsonResponse
+    {
+        // Пример данных. Здесь можно интегрировать реальную базу данных
+        $cars = [
+            [
+                'id' => 1,
+                'brand' => [
+                    'id' => 1,
+                    'name' => 'Toyota',
+                ],
+                'photo' => 'https://example.com/photos/car1.jpg',
+                'price' => 20000,
+            ],
+            [
+                'id' => 2,
+                'brand' => [
+                    'id' => 2,
+                    'name' => 'Honda',
+                ],
+                'photo' => 'https://example.com/photos/car2.jpg',
+                'price' => 25000,
+            ],
+        ];
+
+        return new JsonResponse($cars);
+    }
+}
